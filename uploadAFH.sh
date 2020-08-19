2e798cae1182cd03e2a92c4801d4d70334c27417
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2020-08-19 18:53:46 +0600

    Update uploadAFH.sh

--- uploadAFH.sh
+++ uploadAFH.sh
@@ -2,17 +2,16 @@
 # memberamd @amdserver
 
 
-FILE=<file_name>
+FILE=ubuntu.sh
 export FILE
 hst=uploads.androidfilehost.com
-us=iyke
-key=HsHXIgOLoRXs
+us=Apon77
+key=btRjTD2597P8
 echo "FILE_NAME : $FILE"
-echo " UPLOADER : Zackie Ai"
+echo " UPLOADER : Khalakuzzaman Apon"
 echo ""
 echo "FILE_UPLOAD_IN_PROGRESS ..."
 curl -ftp-pasv -T $FILE ftp://$us:$key@$hst
 echo ""
 echo " UPLOAD_COMPLETED !!!"
-
 
